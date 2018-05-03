64407696759380816bd2fee1993368c3f229b857
Rex <rexhsu@goodmusic-corp.com>
2018-05-03 09:56:12 +0800

    Initial Commit

---
+++ PushServiceDemoTests/PushServiceDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  PushServiceDemoTests.swift
+//  PushServiceDemoTests
+//
+//  Created by Rex on 2018/5/3.
+//  Copyright © 2018年 maudowan. All rights reserved.
+//
+
+import XCTest
+@testable import PushServiceDemo
+
+class PushServiceDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
